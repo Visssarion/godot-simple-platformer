5764f59af5f70942dc0fc1d92fdda50eb1339345
Vissa <46628714+Visssarion@users.noreply.github.com>
2025-07-06 00:49:19 +0300

    Add Camera

---
+++ scenes/entities/camera/camera.gd
@@ -0,0 +1,13 @@
+extends Camera2D
+
+@export var target_group: StringName
+var target_node: Node2D
+
+func _ready() -> void:
+	_find_node.call_deferred()
+
+func _find_node() -> void:
+	target_node = get_tree().get_first_node_in_group(target_group)
+
+func _process(delta: float) -> void:
+	self.position = target_node.position
